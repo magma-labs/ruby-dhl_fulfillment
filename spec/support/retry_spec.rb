503783cfbd38c9c9392d21471d4b0834f44e921b
Kevin Perez <kevin.perez@magmalabs.io>
2017-10-07 00:38:37 -0500

    add spec for retry module

---
+++ spec/support/retry_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+RSpec.describe Retry do
+  let(:test_object) { spy }
+
+  before do
+    self.extend Retry
+  end
+
+  context 'when the running block calls next_try!' do
+    it 'keeps retrying until it runs out of retry attempts' do
+      attempt(5).times do
+        begin
+          test_object.do_something
+          next_try!
+        rescue Retry::OutOfAttempts
+          test_object.do_other_thing
+        end
+      end
+
+      expect(test_object).to have_received(:do_something).exactly(5).times
+    end
+
+    context 'when it runs out of retry attempts' do
+      it 'raises an exception' do
+        block = Proc.new do
+          attempt(5).times do
+            test_object.do_something
+            next_try!
+          end
+        end
+
+        expect(&block).to raise_error Retry::OutOfAttempts
+      end
+    end
+  end
+
+  context 'when the running block doesnt call next_try!' do
+    before do
+      allow(test_object).to receive(:do_something) { 'value' }
+    end
+
+    it 'stops executing the block' do
+      attempt(5).times { test_object.do_something }
+      expect(test_object).to have_received(:do_something).once
+    end
+
+    it 'returns the block result' do
+      result = attempt(5).times { test_object.do_something }
+      expect(result).to eql 'value'
+    end
+  end
+end
